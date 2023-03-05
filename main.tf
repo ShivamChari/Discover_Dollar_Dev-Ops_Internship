1e3983e57148e2f7edd1e331b27860f0390a1af4
ShivamChari <114586379+ShivamChari@users.noreply.github.com>
2023-03-05 16:19:18 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,219 @@
+# provider and profile
+provider "aws" {
+  profile = "ShivamChari"
+  region  = "ap-south-1"
+}
+
+# key variable
+variable "key_name" {
+  default = "ec2Key"
+}
+
+# base_path variable
+variable "base_path" {
+  default = "/home/ShivamChari/terra-infra/"
+}
+
+# private key 
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# aws keypair
+resource "aws_key_pair" "key_pair" {
+  depends_on = [tls_private_key.private_key]
+  key_name   = var.key_name
+  public_key = tls_private_key.private_key.public_key_openssh
+}
+
+# save privateKey
+resource "local_file" "saveKey" {
+  depends_on = [aws_key_pair.key_pair]
+  content = tls_private_key.private_key.private_key_pem
+  filename = "${var.base_path}${var.key_name}.pem"
+}
+
+# vpc
+resource "aws_vpc" "vpc" {
+  cidr_block       = "192.168.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "my-vpc"
+  }
+
+  enable_dns_hostnames = true
+}
+
+# public subnet
+resource "aws_subnet" "public_subnet" {
+  depends_on = [
+    aws_vpc.vpc,
+  ]
+
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "192.168.0.0/24"
+
+  availability_zone_id = "aps1-az1"
+
+  tags = {
+    Name = "public-subnet"
+  }
+
+  map_public_ip_on_launch = true
+}
+
+# private subnet
+resource "aws_subnet" "private_subnet" {
+  depends_on = [
+    aws_vpc.vpc,
+  ]
+
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "192.168.1.0/24"
+
+  availability_zone_id = "aps1-az3"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+# internet gateway
+resource "aws_internet_gateway" "internet_gateway" {
+  depends_on = [
+    aws_vpc.vpc,
+  ]
+
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "internet-gateway"
+  }
+}
+
+# route table with target as internet gateway
+resource "aws_route_table" "IG_route_table" {
+  depends_on = [
+    aws_vpc.vpc,
+    aws_internet_gateway.internet_gateway,
+  ]
+
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "IG-route-table"
+  }
+}
+
+# associate route table to public subnet
+resource "aws_route_table_association" "associate_routetable_to_public_subnet" {
+  depends_on = [
+    aws_subnet.public_subnet,
+    aws_route_table.IG_route_table,
+  ]
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.IG_route_table.id
+}
+
+# elastic ip
+resource "aws_eip" "elastic_ip" {
+  vpc      = true
+}
+
+# associate route table to private subnet
+resource "aws_route_table_association" "associate_routetable_to_private_subnet" {
+  depends_on = [
+    aws_subnet.private_subnet,
+    aws_route_table.NAT_route_table,
+  ]
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.NAT_route_table.id
+}
+
+# NAT gateway
+resource "aws_nat_gateway" "nat_gateway" {
+  depends_on = [
+    aws_subnet.private_subnet,
+    aws_eip.elastic_ip,
+  ]
+  allocation_id = aws_eip.elastic_ip.id
+  subnet_id     = aws_subnet.private_subnet.id
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+# route table with target as NAT gateway
+resource "aws_route_table" "NAT_route_table" {
+  depends_on = [
+    aws_vpc.vpc,
+    aws_nat_gateway.nat_gateway,
+  ]
+
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+
+  tags = {
+    Name = "NAT-route-table"
+  }
+}
+
+# VM security group
+resource "aws_security_group" "sg_VM" {
+  depends_on = [
+    aws_vpc.vpc,
+  ]
+  name        = "sg_VM"
+  description = "Allow VM inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# VM ec2 instance
+resource "aws_instance" "VM" {
+  depends_on = [
+    aws_security_group.sg_VM,
+    aws_nat_gateway.nat_gateway,
+    aws_route_table_association.associate_routetable_to_private_subnet,
+  ]
+  ami = "ami-0732b62d310b80e97"
+  instance_type = "t2.micro"
+  key_name = var.key_name
+  vpc_security_group_ids = [aws_security_group.sg_VM.id]
+  subnet_id = aws_subnet.private_subnet.id
+  user_data = file("configure_VM.sh")
+  tags = {
+      Name = "VM-instance"
+  }
+}
+ user_data =  <<-EOF
+               #!/bin/bash
+               ping 192.168.1.0/24
+               EOF
+}
